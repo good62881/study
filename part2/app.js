1c19502f606e0c107a08fef3c84fb84226b60c80
good62881 <103432847@qq.com>
2018-02-05 16:05:29 +0800

    修改

--- part2/app.js
+++ part2/app.js
@@ -25,7 +25,7 @@ var MongoStore = require('connect-mongo')(session);
 var sessionMiddleware = session({
     secret: 'loginIn',
     resave: true,    //必填，为true时强制更新。为false，除非session修改，否则不更新。（前提是没有调用或自己实现touch()事件）
-    rolling: true,  //使用原设定的maxAge重设session过期时间
+    rolling: true,  //使用设定的maxAge重设session过期时间
     saveUninitialized: false,   //必填，为true时，新生成的session直接写入Mongo中。为false时，新生成但是没有任何修改的session不会写入Mongo。（例如登录，登录后会立马添加session.user，然后被写入。）
     cookie: { maxAge: 10*60*1000  },
     store: new MongoStore({
